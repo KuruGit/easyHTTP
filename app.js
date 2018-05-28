0f1a8e363e8e9f7d6229554918a4e846a9c8e801
David <david.breier@outlook.de>
2018-05-28 05:29:12 +0200

    added post request
    added the post request. Somehow does not show results in console. will have to look into that

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const http = new EasyHTTP();
 
 //new get
-http.get("https://jsonplaceholder.typicode.com/users").then(data => console.log(data)).catch(err=>console.log(err));
+// http.get("https://jsonplaceholder.typicode.com/users").then(data => console.log(data)).catch(err=>console.log(err));
 
+const data = {
+    name: "John Doe",
+    username: "jdoe",
+    email: "jdoe@gmail.com"
+}
+
+http.get("https://jsonplaceholder.typicode.com/users",data)
+.then(data => console.log(data))
+.catch(err=>console.log(err));
--- easyHTTP.js
+++ easyHTTP.js
@@ -9,14 +9,29 @@
  ***********************************/
 
 class EasyHTTP {
+
     //Make http get request
     get(url) {
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve, reject) => {
             fetch(url)
             .then(res => res.json())
             .then(data => resolve(data))
             .catch(err => reject (err));
+            });
+        }
+//Make http post request
+post (url, data){
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method:"POST",
+            headers: {
+                "Content-type":"application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject (err));
         });
-        
     }
 }
